1db685839e01a04cfcb5393ab5062468714e4565
Shreyas <pshreyas2827@gmail.com>
2023-07-21 18:14:56 +0530

    updated

--- message.js
+++ message.js
@@ -1,63 +1,52 @@
-// Fetch messages from the API
-async function fetchMessages() {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const messages = await response.json();
-      return messages;
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-      return [];
-    }
-  }
-  
-  // Function to display messages in the chat box
-  async function displayMessages() {
+const socket = new WebSocket('ws://localhost:8080'); // Replace with your WebSocket server URL
+
+const currentUser = {
+  id: Math.random().toString(36).substr(2, 9),
+  name: "User" + Math.floor(Math.random() * 100), // Random name for demo
+};
+
+socket.onopen = () => {
+  console.log('WebSocket connection established.');
+};
+
+socket.onmessage = (event) => {
+  const message = JSON.parse(event.data);
+  displayMessage(message);
+};
+
+// Function to display messages in the chat box
+function displayMessage(message) {
+  const chatBox = document.getElementById("chat-box");
+  const messageDiv = document.createElement("div");
+  messageDiv.textContent = `${message.sender}: ${message.body}`;
+  messageDiv.classList.add("message");
+  chatBox.appendChild(messageDiv);
+  chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom of the chat
+}
+
+// Function to send a message
+function sendMessage() {
+  const messageInput = document.getElementById("message-input");
+  const message = messageInput.value.trim();
+
+  if (message !== "") {
     const chatBox = document.getElementById("chat-box");
-    chatBox.innerHTML = ""; // Clear the chat box before updating messages
-  
-    const messages = await fetchMessages();
-  
-    messages.forEach(message => {
-      const messageDiv = document.createElement("div");
-      messageDiv.textContent = message.body;
-  
-      if (message.userId === currentUser.id) {
-        messageDiv.classList.add("sent-message");
-      } else {
-        messageDiv.classList.add("received-message");
-      }
-  
-      chatBox.appendChild(messageDiv);
-    });
-  
+    const messageDiv = document.createElement("div");
+    messageDiv.textContent = `${currentUser.name}: ${message}`;
+    messageDiv.classList.add("sent-message");
+    chatBox.appendChild(messageDiv);
     chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom of the chat
+
+    // Send the message to the WebSocket server
+    socket.send(JSON.stringify({ sender: currentUser.name, body: message }));
+
+    messageInput.value = ""; // Clear the input after sending the message
   }
-  
-  // Function to send a message
-  function sendMessage() {
-    const messageInput = document.getElementById("message-input");
-    const message = messageInput.value.trim();
-  
-    if (message !== "") {
-      // Simulate sending the message by directly adding it to the chat box
-      const chatBox = document.getElementById("chat-box");
-      const messageDiv = document.createElement("div");
-      messageDiv.textContent = message;
-      messageDiv.classList.add("sent-message");
-      chatBox.appendChild(messageDiv);
-  
-      messageInput.value = ""; // Clear the input after sending the message
-      chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom of the chat
-    }
+}
+
+// Listen for Enter key press to send the message
+document.getElementById("message-input").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+    sendMessage();
   }
-  
-  // Listen for Enter key press to send the message
-  document.getElementById("message-input").addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-      sendMessage();
-    }
-  });
-  
-  // On page load, fetch and display messages
-  displayMessages();
-  
+});
---
+++ server.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('Received:', message);
+
+    // Broadcast the message to all connected clients (except the sender)
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('WebSocket server is listening on port 8080');
